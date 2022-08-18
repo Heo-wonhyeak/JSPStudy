edca3554d5274965d2f3a97cfa33555ad70adeb0
Heo-wonhyeak <102012064+Heo-wonhyeak@users.noreply.github.com>
2022-08-18 17:18:46 +0900

    chap15

---
+++ chap15_BoardModel2 (6)/src/main/java/kr/co/ezenac/model2/board/EditController.java
@@ -0,0 +1,18 @@
+package kr.co.ezenac.model2.board;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/edit.do")
+public class EditController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+}
